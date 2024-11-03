84a875ccab8426971cd401e23c1efd72f3dd524f
Sanna Tikkakoski <153513791+sannatikk@users.noreply.github.com>
2024-11-03 16:55:06 +0200

    db environment condition fixed

--- server/helpers/db.js
+++ server/helpers/db.js
@@ -11,7 +11,7 @@ const openDb = () => {
     const pool = new Pool ({
         user: process.env.DB_USER,
         host: process.env.DB_HOST,
-        database: environment === 'development' ? process.env.DB_NAME : process.env.TEST_DB_NAME,
+        database: environment === 'development' ? process.env.TEST_DB_NAME : process.env.DB_NAME,
         password: process.env.DB_PASSWORD,
         port: process.env.DB_PORT
     })
